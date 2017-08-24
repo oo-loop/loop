0570e3d809b27b62f5a1f736f6dcb1eee35195a1
Eric Seigers <eric@gplusmedia.com>
2017-08-24 16:21:59 +0900

    some documentation view

---
+++ docs/src/components/documentation/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<component v-for="item in listing" :is="item" :key="item"></component>
+	</div>
+</template>
+
+<script>
+import type from './type'
+
+export default {
+	name: 'Documentation',
+	components: {
+		type
+	},
+	props: {
+	},
+	data: () => ({
+		listing: [
+			'type'
+		]
+	}),
+	methods: {
+	},
+}
+</script>
+<style lang="scss">
+	
+</style>
---
+++ docs/src/components/documentation/type/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h2>Typography</h2>
+		<div>
+			<p v-for="n in 8" :class="`is-size-${n}`">.is-size-{{n}}</p>			
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Type',
+	props: {
+	},
+	data: () => ({
+	}),
+	methods: {
+	},
+}
+</script>
+<style lang="scss">
+	
+</style>
