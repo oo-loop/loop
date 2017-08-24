6a12bfed69c5d0ce7d28aee9acf508c2cb22375c
Eric Seigers <eric@gplusmedia.com>
2017-08-24 15:10:57 +0900

    added package.json and doc setup

---
+++ docs/src/components/Hello.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>RING</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "~ring/ring";
+</style>
