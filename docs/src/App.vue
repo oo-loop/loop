8f3da8f312425ab7c244f3fa70495af303d9b2fe
Eric Seigers <eric@gplusmedia.com>
2017-08-30 09:16:34 +0900

    Auto stash before merge of "dev" and "origin/dev"

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -14,6 +14,6 @@ export default {
 </script>
 <style lang="scss">
 	@import '~loop/loop';
-    // @import '../node_modules/code-prettify/loader/skins/desert.css';
-    @import '../node_modules/code-prettify/src/prettify.css';
+    // @import '~code-prettify/loader/skins/desert.css';
+    @import '~code-prettify/src/prettify.css';
 </style>
