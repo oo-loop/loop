c4585aeb18a5679256f2ce08c72bbacd19ba091b
Michael Vurchio <michael@gplusmedia.com>
2017-09-22 19:27:04 +0900

    buttons w..ip

--- docs/src/components/documentation/button/index.vue
+++ docs/src/components/documentation/button/index.vue
@@ -1,11 +1,24 @@
 <template>
 	<div>
 		<h1 class="title">Button</h1>
-		<p class="text-large">Action button of different sizes</p>
-		<a class="btn text-tiny">button</a>
-		<a class="btn text-small">button</a>
-		<a class="btn">button</a>
-		<a class="btn text-large">button</a>
+		<p>Alter the size of the button by applying any of the <code class="text-info">$text-sizes</code> classes on your <code>.button</code></p>
+		<p><a class="button text-tiny">tiny</a>
+		<a class="button text-small">small</a>
+		<a class="button">button</a>
+		<a class="button text-large">large</a></p>
+		<pretty-code :code="html.size"></pretty-code>
+
+		<p>Change the color of the button through the modifiers <code>-$colorname</code> present in the variable <code class="text-info">$btn-colors</code>.<br>
+		By default <code>-primary</code> <code>-secondary</code> <code>-success</code> <code>-warning</code> <code>-danger</code></p>
+
+		<p>
+		<a class="button -primary">primary</a>
+		<a class="button -secondary">secondary</a>
+		<a class="button -success">success</a>
+		<a class="button -warning">warning</a>
+		<a class="button -danger">danger</a>
+		</p>
+
 	</div>
 </template>
 
@@ -20,7 +33,10 @@ export default {
 		markup
 	},
 	data: () => ({
-		html: {}
+		html: {
+			size: require('./code/size.html'),
+			color: require('./code/color.html'),
+		}
 	}),
 	methods: {
 	}
