6dfb50c0bc90c2f9125b7591c7fc200315b0fd42
Michael Vurchio <michael@gplusmedia.com>
2017-10-02 18:13:47 +0900

    border with color

--- docs/src/components/documentation/visibility/index.vue
+++ docs/src/components/documentation/visibility/index.vue
@@ -1,62 +1,63 @@
 <template>
 	<div>
 		<h1 class="title">Visibility</h1>
-		<p class="text-large">Hide elements through the available breakpoints</p>
+		<p class="text-large">Hide elements from the available breakpoints</p>
+		<p>Use the classes <code>.hidden</code> <code>.hidden@sm</code> <code>.hidden@md</code> <code>.hidden@lg</code></p>
 		<div class="bg-secondary wrapper-tiny text-center">
 			<div class="columns -gutter-tiny">
 				<div class="column">
-					<div class="text-primary bordered -primary">.hidden</div>
+					<div class="text-primary border-primary">.hidden</div>
 				</div>
 				<div class="column">
-					<div class="text-primary bordered -primary">.hidden</div>
+					<div class="text-primary border-primary">.hidden</div>
 				</div>
 				<div class="column">
-					<div class="text-primary bordered -primary">.hidden</div>
+					<div class="text-primary border-primary">.hidden</div>
 				</div>
 				<div class="column">
-					<div class="text-primary bordered -primary">.hidden</div>
-				</div>
-			</div>
-			<div class="columns -gutter-tiny">
-				<div class="column">
-					<div class="bg-primary bordered -primary"><br></div>
-				</div>
-				<div class="column">
-					<div class="text-primary bordered -primary">.hidden@sm</div>
-				</div>
-				<div class="column">
-					<div class="text-primary bordered -primary">.hidden@sm</div>
-				</div>
-				<div class="column">
-					<div class="text-primary bordered -primary">.hidden@sm</div>
+					<div class="text-primary border-primary">.hidden</div>
 				</div>
 			</div>
 			<div class="columns -gutter-tiny">
 				<div class="column">
-					<div class="bg-primary bordered -primary"><br></div>
+					<div class="bg-primary border-primary"><br></div>
 				</div>
 				<div class="column">
-					<div class="bg-primary bordered -primary"><br></div>
+					<div class="text-primary border-primary">.hidden@sm</div>
 				</div>
 				<div class="column">
-					<div class="text-primary bordered -primary">.hidden@md</div>
+					<div class="text-primary border-primary">.hidden@sm</div>
 				</div>
 				<div class="column">
-					<div class="text-primary bordered -primary">.hidden@md</div>
+					<div class="text-primary border-primary">.hidden@sm</div>
+				</div>
+			</div>
+			<div class="columns -gutter-tiny">
+				<div class="column">
+					<div class="bg-primary border-primary"><br></div>
+				</div>
+				<div class="column">
+					<div class="bg-primary border-primary"><br></div>
+				</div>
+				<div class="column">
+					<div class="text-primary border-primary">.hidden@md</div>
+				</div>
+				<div class="column">
+					<div class="text-primary border-primary">.hidden@md</div>
 				</div>
 			</div>
 			<div class="columns -gutter-tiny -v-gutter-less">
 				<div class="column">
-					<div class="bg-primary bordered -primary"><br></div>
+					<div class="bg-primary border-primary"><br></div>
 				</div>
 				<div class="column">
-					<div class="bg-primary bordered -primary"><br></div>
+					<div class="bg-primary border-primary"><br></div>
 				</div>
 				<div class="column">
 					<div class="bg-primary -primary"><br></div>
 				</div>
 				<div class="column">
-					<div class="text-primary bordered -primary">.hidden@lg</div>
+					<div class="text-primary border-primary">.hidden@lg</div>
 				</div>
 			</div>
 		</div>
