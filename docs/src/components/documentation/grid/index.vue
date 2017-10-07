6f5a42fbf2803b272036c05556d422883bbe19c8
Michael Vurchio <michael@gplusmedia.con>
2017-10-07 18:53:58 +0900

    grid review

--- docs/src/components/documentation/grid/index.vue
+++ docs/src/components/documentation/grid/index.vue
@@ -1,47 +1,37 @@
 <template>
 	<div>
 		<h1 class="title">Grid Layout <small class="text-small text-danger">alpha</small></h1>
+		<p class="text-large">Based on CSS grid, using the 12 grid system (columns and rows).</p>
 
-		<h3 id="regular"><a href="#regular">#</a> Regular grid</h3>
-		<template v-for="n in 12" >
-			<div class="grid">
-				<div :class="`grid-item -col-${n}`">
-					<p class="cell">.-col-{{n}}</p>
-				</div>
-			</div>
-		</template>
-
-		<pretty-code :code="html.regular" />
-
-		<h3 id="responsive"><a href="#responsive">#</a> Responsive Grids</h3>
+		<h3 id="responsive"><a href="#responsive">#</a> Responsive Columns and Rows</h3>
 		<div class="grid">
-			<div class="grid-item -col-3"><p class="cell">.-col-3</p></div>
-			<div class="grid-item -col-9"><p class="cell">.-col-9</p></div>
-			<div class="grid-item -col-4 -col-xs-12"><p class="cell">.-col-4.-col-xs-12</p></div>
-			<div class="grid-item -col-4 -col-md-4"><p class="cell">.-col-4</p></div>
-			<div class="grid-item -col-4"><p class="cell">.-col-4</p></div>
-			<div class="grid-item -col-2"><p class="cell">.-col-2</p></div>
-			<div class="grid-item -col-5 -col-lg-12"><p class="cell">.-col-5</p></div>
-			<div class="grid-item -col-3"><p class="cell">.-col-3</p></div>
-			<div class="grid-item -col-2"><p class="cell">.-col-2</p></div>
+			<div class="cell -col-6 -row-3 -col-4@sm">1</div>
+			<div class="cell -col-6 -col-8@sm">2</div>
+			<div class="cell -col-6 -col-4@sm">3</div>
+			<div class="cell -col-6 -col-4@sm -row-2@sm -row-3@md -row-4@lg">4</div>
+			<div class="cell -col-4">5</div>
+			<div class="cell -col-4 -col-2@lg">6</div>
+			<div class="cell -col-4 -row-2@md -row-1@lg">7</div>
+			<div class="cell -col-12 -col-4@sm -col-2@lg">8</div>
+			<div class="cell -col-12 -col-4@md -col-8@lg">9</div>
 		</div>
 
-		<pretty-code :code="html.sample" />
+		<pretty-code :code="html.responsive" />
 
 		<h3 id="gaps"><a href="#gaps">#</a> Grip Gaps</h3>
 		<div>
 			<button v-for="n in 15" @click="gap = n"> gap size {{n}}</button>
 		</div>
 		<div class="grid" :class="[gapClass]">
-			<div class="grid-item -col-3"><p class="cell">.-col-3</p></div>
-			<div class="grid-item -col-9"><p class="cell">.-col-9</p></div>
-			<div class="grid-item -col-4 -col-xs-12"><p class="cell">.-col-4.-col-xs-12</p></div>
-			<div class="grid-item -col-4"><p class="cell">.-col-4</p></div>
-			<div class="grid-item -col-4"><p class="cell">.-col-4</p></div>
-			<div class="grid-item -col-2"><p class="cell">.-col-2</p></div>
-			<div class="grid-item -col-5"><p class="cell">.-col-5</p></div>
-			<div class="grid-item -col-3"><p class="cell">.-col-3</p></div>
-			<div class="grid-item -col-2"><p class="cell">.-col-2</p></div>
+			<div class="cell -col-3"></div>
+			<div class="cell -col-9"></div>
+			<div class="cell -col-4"></div>
+			<div class="cell -col-4"></div>
+			<div class="cell -col-4"></div>
+			<div class="cell -col-2"></div>
+			<div class="cell -col-5"></div>
+			<div class="cell -col-3"></div>
+			<div class="cell -col-2"></div>
 		</div>
 
 	</div>
@@ -61,7 +51,7 @@ export default {
 		gap: 10,
 		html: {
 			regular: require('./code/regular-grid.html'),
-			sample: require('./code/sample.html'),
+			responsive: require('./code/responsive.html'),
 			gaps: '',
 		}
 	}),
@@ -76,11 +66,12 @@ export default {
 </script>
 <style lang="scss" scoped>
 	@import "~loop/variables";
+
 	.cell {
-		margin-bottom: 10px;
-		text-align: center;
+		padding: 1rem;
 		background-color: $color-primary;
 	}
+
 	button {
 	  display: inline-block;
 	  -webkit-appearance: none;
