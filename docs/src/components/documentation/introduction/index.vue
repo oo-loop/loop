7abd0426c85042227a14e15d528b0fbc77a606ab
Michael Vurchio <michael@gplusmedia.con>
2017-10-22 18:10:32 +0900

    intro doc update

--- docs/src/components/documentation/introduction/index.vue
+++ docs/src/components/documentation/introduction/index.vue
@@ -10,6 +10,13 @@
         Loop aims for flexibility and customization allowing the addition and removal of rules through variables.<br>
         The goal is to use the strict minimum, keeping the css file light and small without a bunch of superfluous rules and components.
     </p>
+    <p>Loop differentiates components and utilities with semantic for more clarity<br>
+      The loop attribute for component<code class="color-secondary">oo-componentName</code><br>
+      The class attribute for utility<code>.utilityName</code>.</p>
+    <p><strong>Beware</strong><br>The loop attribute is not a valid html5 attribute.<br>
+      If it is important for you, set the variable <code class="color-info">$loop-component-html5-validity</code> to true<br>
+      Loop components will be then avaiable through the data attribute <code class="color-secondary">data-oo-componentName</code>.
+    </p>
   </div>
 </template>
 
