0365836257c9da613055818556e8f9aaf224304a
Michael Vurchio <michael@gplusmedia.com>
2017-10-23 13:11:50 +0900

    doc text

--- docs/src/components/documentation/introduction/index.vue
+++ docs/src/components/documentation/introduction/index.vue
@@ -4,17 +4,19 @@
       <logo class="color-primary logo" />
     </transition>
     <h1 class="h2">Loop</h1>
-    <p class="text-large">"Why going into Circles?"</p>
+    <p class="text-large">"Why going in Circles?"</p>
     <p><strong>v 0.2.0 Alpha</strong></p>
     <p>
         Loop aims for flexibility and customization allowing the addition and removal of rules through variables.<br>
         The goal is to use the strict minimum, keeping the css file light and small without a bunch of superfluous rules and components.
     </p>
-    <p>Loop differentiates components and utilities with semantic for more clarity<br>
-      The loop attribute for component<code class="color-secondary">oo-componentName</code><br>
-      The class attribute for utility<code>.utilityName</code>.</p>
+    <p>Loop differentiates components and utilities with semantic for more clarity</p>
+    <ul class="list-unstyle list-center">
+        <li><code class="color-secondary">oo-componentName</code>loop attribute for component</li>
+        <li><code>.utilityName</code>class attribute for utility.</li>
+    </ul>
     <p><strong>Beware</strong><br>The loop attribute is not a valid html5 attribute.<br>
-      If it is important for you, set the variable <code class="color-info">$loop-component-html5-validity</code> to true<br>
+      If it does concern you, set the variable <code class="color-info">$loop-component-html5-validity</code> to true<br>
       Loop components will be then avaiable through the data attribute <code class="color-secondary">data-oo-componentName</code>.
     </p>
   </div>
