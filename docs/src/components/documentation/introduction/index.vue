075b1829ea6c65d8ebbe916f9da2757a39ac548e
Michael Vurchio <michael.vurchio@gmail.com>
2018-02-24 20:19:17 +0900

    update intro

--- docs/src/components/documentation/introduction/index.vue
+++ docs/src/components/documentation/introduction/index.vue
@@ -4,24 +4,44 @@
       <logo class="color-primary logo" />
     </transition>
     <h1 class="h2">Loop</h1>
-    <p class="text-large">"Why go in circles?"</p>
+    <p class="text-large font-italic">"Why go in circles?"</p>
+    <h2 class="h4 mb-20">CSS framework for Front-end Developer</h2>
     <p><a href="https://github.com/oo-loop/loop" title="See on GitHub"><img src="/static/assets/github.png" height="20" alt="GitHub Page"></a>
     <a class="ml-10" href="https://www.npmjs.com/package/oo-loop" title="See on NPM"><img src="https://img.shields.io/npm/v/oo-loop.svg?style=flat-square" alt="Latest version on NPM"></a></p>
-    <p>
-        Loop wants to reconsider the way CSS is created, aiming to make it more maintainable, readable and expressive.<br>
-        Loop works towards flexibility and customization without a bunch of superfluous rules and components.<br>
-        Not opinionated about any style and based on variables, it allows the addition and removal of rules and properties with ease.<br>
-        Being like a partner for CSS development, it assists the developer matching their styles with their design needs.
-    </p>
-    <p>Loop differentiates components and utilities with semantic for more clarity</p>
-    <ul class="list-unstyle list-center">
-        <li><code class="color-secondary">oo-componentName</code>loop attribute for component</li>
-        <li><code>.utilityName</code>class attribute for utility.</li>
-    </ul>
-    <p><strong>Beware</strong><br>The loop attribute is not a valid html5 attribute.<br>
-      If it does concern you, set the variable <code class="color-info">$loop-component-html5-validity</code> to true<br>
-      Loop components will be then available through the data attribute <code class="color-secondary">data-oo-componentName</code>.
-    </p>
+    <div oo-column="row align-center">
+      <div oo-column="col col-10@sm col-8@md col-6@lg">
+        <p>
+          Loop wants to reconsider the way CSS is created and used, aiming to make it more maintainable, readable and expressive.
+        </p>
+        <p>
+          Loop works towards flexibilty and customization to give the developer complete control on his project letting him create his own set of utilities.<br>
+          It is specifically based on variables to allow the addition and removal of rules and properties with ease.
+        </p>
+        <p>
+          Loop is not meant to be a framework providing any possible existing components. Its philosophy is to be non-opinionated and to be more like a companion for CSS development assisting the developer matching their styles with their design needs.
+        </p>
+        <p>
+          Anything specific to the project you're working on is for you to develop.<br>
+          Loop is here to help.
+        </p>
+        
+        <h3 class="h4">Semantic</h3>
+        <p>Loop differentiates components and utilities with semantic for more clarity</p>
+        <ul class="list-unstyle list-center">
+            <li><code class="color-secondary">oo-componentName</code> loop attribute for components</li>
+            <li><code>.utilityName</code> class attribute for utilities.</li>
+        </ul>
+        
+        <h3 class="h4">Beware</h3>
+        <p>The loop attribute is not a valid html5 attribute.<br>
+          If it does concern you, set the variable <code class="color-info">$loop-component-html5-validity</code> to true<br>
+          Loop components will be then available through the data attribute <code class="color-secondary">data-oo-componentName</code>.
+        </p>
+
+        <h3 class="h4">Start</h3>
+        <code class="color-info">yarn add oo-loop</code> or <code class="color-info">npm install oo-loop</code>
+      </div>
+    </div>
   </div>
 </template>
 
